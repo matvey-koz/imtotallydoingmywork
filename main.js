73ed3a43d9b2aea8999755ae92a10680d3c6558f
matvey-koz <m.key.201015@gmail.com>
2024-05-07 15:45:27 -0400

    update

--- main.js
+++ main.js
@@ -5,25 +5,31 @@ function IWillFindYou() {
     // add div with class "form"
     let form = document.createElement("div");
     form.classList.add("form");
-
-
-    let text = document.createElement("p");
-    text.innerText = "Enter the password hehehe:";
-    let input = document.createElement("input");
-    input.setAttribute("type", "password");
+    // add pasword label
+    let label = document.createElement("label")
+    label.innerText = "You really thought you could just see the answers huh? What's the password?";
+    // add submit button container
+    let submitContainer = document.createElement("div");
+    submitContainer.classList.add("submitButton");
+    // add submit button
     let submit = document.createElement("button");
-    submit.innerText = "Click";
-    submit.setAttribute("id", "submitButton");
     submit.setAttribute("onclick", "doNothing()");
-    submit.style.marginTop = "50px";
-    container.append(text);
-    container.append(input);
-    container.append(submit);
-    document.body.appendChild(container);
-    form.style.display = "none";
-}
 
-document.querySelector("#submitButton").addEventListener("click", doNothing());
+    // let text = document.createElement("p");
+    // text.innerText = "Enter the password hehehe:";
+    // let input = document.createElement("input");
+    // input.setAttribute("type", "password");
+    // let submit = document.createElement("button");
+    // submit.innerText = "Click";
+    // submit.setAttribute("id", "submitButton");
+    // submit.setAttribute("onclick", "doNothing()");
+    // submit.style.marginTop = "50px";
+    // container.append(text);
+    // container.append(input);
+    // container.append(submit);
+    // document.body.appendChild(container);
+    // form.style.display = "none";
+}
 
 function doNothing() {
     window.open("mat.x10.mx");
